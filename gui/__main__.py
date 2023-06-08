a1c724ff02048583d4c6088ab2bab090e2456ee1
Chris Wolf <chriswolfdesign@gmail.com>
2023-06-08 18:38:09 -0400

    Built basic gui

---
+++ gui/__main__.py
@@ -0,0 +1,15 @@
+import pygame
+
+if __name__ == "__main__":
+    pygame.init()
+
+    screen = pygame.display.set_mode([500, 500])
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+    pygame.quit()
